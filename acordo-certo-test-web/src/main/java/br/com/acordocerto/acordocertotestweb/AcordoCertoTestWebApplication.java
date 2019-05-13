6e25dffd0a4a00d845337712854d836b3a789164
lsena <luisfjs@msn.com>
2019-05-13 08:40:45 -0300

    Cadastro de Despesas
    Correções de configurações para deploy no tomcat

--- acordo-certo-test-web/src/main/java/br/com/acordocerto/acordocertotestweb/AcordoCertoTestWebApplication.java
+++ acordo-certo-test-web/src/main/java/br/com/acordocerto/acordocertotestweb/AcordoCertoTestWebApplication.java
@@ -1,14 +1,22 @@
 package br.com.acordocerto.acordocertotestweb;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "br.com.acordocerto")
 @EnableJpaRepositories("br.com.acordocerto.domain.repository")
 @EntityScan("br.com.acordocerto.domain.model")
-public class AcordoCertoTestWebApplication {
+public class AcordoCertoTestWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AcordoCertoTestWebApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AcordoCertoTestWebApplication.class, args);
